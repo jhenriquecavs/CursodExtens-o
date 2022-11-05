dd48a2090a56fbcf322a716ac34f86381d143e00
jhenriquecavs <114232200+jhenriquecavs@users.noreply.github.com>
2022-11-05 16:15:41 -0300

    Add files via upload

---
+++ Jh337.java
@@ -0,0 +1,101 @@
+package jh337;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Jh337 {
+	public static void main(String[]args) {
+		// FAÇA UM PROGRAMA QUE PEÇA DOIS NÚMEROS E VERIFIQUE (USANDO IF E ELSE) SE O PRIMEIRO NÚMERO É DIVISÍVEL PELO SEGUNDO: 
+		//		int n1 = Integer.parseInt (JOptionPane.showInputDialog("Digite o primeiro número:"));
+		//		int n2 = Integer.parseInt (JOptionPane.showInputDialog("Digite o segundo número:"));
+		//		if (n1%n2==0)  { 
+		//			JOptionPane.showMessageDialog(null, "É Divisível");
+		//		}else {
+		//			JOptionPane.showMessageDialog(null, "Não é Divisível");
+		//		}
+		//	}
+		//}
+
+		// ESCREVA UM PROGRAMA QUE RECEBE A QUANTIDADE DE LITROS DE COMBUSTÍVEL E APLICA UM DESCONTO DE 5% CASO A QUANTIDADE DE LITROS SEJA MAIOR DO QUE 20. EXIBA O VALOR FINAL; VALOR POR LITRO = R$4,58.:
+
+		//		double preco = 4.58;
+		//		double desconto = Double.parseDouble(JOptionPane.showInputDialog("Digite o desconto (%)"))/100;
+		//		double qntLitros = Double.parseDouble(JOptionPane.showInputDialog("Digite a quantidade de litros"));
+		//		if(qntLitros>20) {
+		//			JOptionPane.showMessageDialog(null, "Valor a pagar: R$ "+(preco=qntLitros*(1-desconto)));
+		//		}else {
+		//			JOptionPane.showMessageDialog(null, "Valor a pagar: R$ "+(preco*qntLitros));
+		//		}
+		//
+		//	}
+		//}
+
+		// CALCULADORA:
+		//		double n1 = Double.parseDouble (JOptionPane.showInputDialog("Digite o primeiro número:"));
+		//		double n2 = Double.parseDouble (JOptionPane.showInputDialog("Digite o segundo número:"));
+		//		char op = JOptionPane.showInputDialog("Digite o operador:") . charAt(0);
+		//		switch (op) {
+		//		case '+':
+		//			JOptionPane.showMessageDialog(null, n1+n2);
+		//			break;
+		//		case '-':
+		//			JOptionPane.showMessageDialog(null, n1-n2);
+		//			break;
+		//		case '/':
+		//			JOptionPane.showMessageDialog(null, n1/n2);
+		//			break;
+		//		case '*':
+		//			JOptionPane.showMessageDialog(null, n1*n2);
+		//			break;
+		//		default:
+		//			JOptionPane.showMessageDialog(null, "Operador inválido!");
+		//			break;
+		// FAÇA UMA CONTAGEM DE 10 IDADES E TIRE A MÉDIA:
+		//		int cont = 0;
+		//		double soma = 0;
+		//		while (cont<5) {
+		//			soma+= Double.parseDouble(JOptionPane.showInputDialog("Digite a idade da pessoa"+(cont+1)));
+		//			cont++;
+		//		}
+		//		JOptionPane.showMessageDialog(null,"A Média das Idades é:" +(soma/5));
+		// FAÇA O USUÁRIO PEDIR UM NÚMERO E VEJA SE ELE É DIVISÍVEL POR ELE MESMO OU POR 1, PARA SABER SE É PRIMO OU NÃO:
+		//		int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número:"));
+		//		boolean ehPrimo = true;
+		//		for (int i = 2; i < n; i++) {
+		//			if(n%i==0) {
+		//				ehPrimo=false;
+		//				break;
+		//			}
+		//		}
+		//		if(n<2)
+		//			ehPrimo=false;
+		//		if(ehPrimo)
+		//			JOptionPane.showMessageDialog(null, "O número é primo!");
+		//		else
+		//			JOptionPane.showMessageDialog(null, "O número não é primo!");
+		// FAZER UM QUADRADO COM O INTERIOR VAZIO, SÓ COM AS BORDAS:
+		//		int n = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+		//		for (int i=0; i < n; i++) {
+		//			for (int j = 0; j < n; j++) {
+		//				if(i==0 || i==n-1 || j==0 || j==n-1 || j==i || i==n-1-j)
+		//				   System.out.print("x"); 
+		//				else
+		//				   System.out.print(" "); 	
+		//			}
+		//			System.out.println("");
+		//		}
+	}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
